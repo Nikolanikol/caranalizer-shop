ab74469436e42cf97bb7bc0dd961d8aa8a53d801
Николай Борис <krezi3927@gmail.com>
2026-06-24 22:30:20 +0900

    fix: move analytics scripts to body, add noscript fallback for Metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,8 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning className={spaceGrotesk.variable}>
-      <head>
+      <body>
+        {children}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-7MZ9ET3VPK"
           strategy="afterInteractive"
@@ -41,10 +42,14 @@ export default function RootLayout({
           {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-7MZ9ET3VPK');`}
         </Script>
         <Script id="yandex-metrika" strategy="afterInteractive">
-          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108825981','ym');ym(108825981,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});`}
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108825981','ym');ym(108825981,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true,ecommerce:"dataLayer"});`}
         </Script>
-      </head>
-      <body>{children}</body>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/108825981" style={{position:'absolute',left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+      </body>
     </html>
   );
 }
